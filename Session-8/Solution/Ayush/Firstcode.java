1c95083c88b833124cfb61a631c83c430ca94885
Ayush Ravi <ayushravi8087@gmail.com>
2024-11-28 16:34:03 +0000

    Declaration of Variables with appropriate values

---
+++ Session-8/Solution/Ayush/Firstcode.java
@@ -0,0 +1,23 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        byte bytevalue = 12;
+        short shortvalue = 30500;
+        int intvalue = 987654;
+        long longvalue = 123456789L;
+        float floatvalue = 887.34f;
+        double doublevalue = 12345.6789;
+        char charvalue = 'X';
+        boolean booleanvalue = false;
+        String stringvalue = "Ayush's Assignment";
+
+        System.out.println("byte: " + bytevalue);
+        System.out.println("short: " + shortvalue);
+        System.out.println("int: " + intvalue);
+        System.out.println("long: " + longvalue);
+        System.out.println("float: " + floatvalue);
+        System.out.println("double: " + doublevalue);
+        System.out.println("char: " + charvalue);
+        System.out.println("boolean: " + booleanvalue);
+        System.out.println("string: " + stringvalue);
+    }
+}
