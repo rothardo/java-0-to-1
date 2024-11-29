6e6fc4a0e2c7196b99b5e9da6583a4be0d6a3268
Amit Raj <amit712006raj@gmail.com>
2024-11-29 15:17:53 +0000

    this is my first java code

---
+++ Session-8/Solution/amit/first_code_in_java.java
@@ -0,0 +1,45 @@
+public class first_code_in_java {
+    public static void main(String[] args) {
+        byte byte_value = 121;
+        short short_value = 8799;
+        int int_value = 545946;
+        // IN LOGN VALUE WE HAVE TO WRITE THE 'L' AFTER ENDING THE VALUE OTHERWISE IS IS CONSIDERED AS INTEGER.
+        long long_value = 123456789L;
+
+        // IN FLOAT VALUE WE HAVE TO WRITE 'f'.
+        float float_value = 499.34f;
+
+        double double_value = 1789.989;
+
+        char char_value = 'k';
+        boolean boolen_value = false;
+        String string_value = "HELLO AMIT";
+
+        System.out.print("Byte value: ");
+        System.out.println(byte_value);
+
+        System.out.print("Short value: ");
+        System.out.println(short_value);
+
+        System.out.print("Integer value: ");
+        System.out.println(int_value);
+
+        System.out.print("Long value: ");
+        System.out.println(long_value);
+
+        System.out.print("Float value: ");
+        System.out.println(float_value);
+
+        System.out.print("Double value: ");
+        System.out.println(double_value);
+
+        System.out.print("Character value: ");
+        System.out.println(char_value);
+
+        System.out.print("Boolean value: ");
+        System.out.println(boolen_value);
+
+        System.out.print("String value: ");
+        System.out.println(string_value);
+    }
+}
