e0defd14e21156ccf03af3d1e5bcb17570b540d8
Amit Raj <amit712006raj@gmail.com>
2024-12-03 15:58:12 +0000

    updated logic in max of three

--- Session-10/Solutions/amit/max_of_three.java
+++ Session-10/Solutions/amit/max_of_three.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
-public class max_of_three {
-    public static void main(String[] args){
-        Scanner sc =new Scanner (System.in);
-        System.out.print("ENTER THE NO 1 : ");
-        int num1=sc.nextInt();
-        System.out.print("ENTER THE NO 2 : ");
-        int num2=sc.nextInt();
-        System.out.print("ENTER THE NO 3 : ");
-        int num3=sc.nextInt();
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        if(num1>num2 && num2>num3)
-        {
-            System.out.println("THE GREATEST IS "+ num1);
-        }
-        else if(num2>num3 && num3>num1)
-        {
-            System.out.println("THE GREATEST IS "+ num2);
-        }
-        else
-        {
-            System.out.println("THE GREATEST IS "+ num3);
+        System.out.print("ENTER THE NUMBER 1: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("ENTER THE NUMBER 2: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("ENTER THE NUMBER 3: ");
+        int num3 = sc.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("THE GREATEST IS " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("THE GREATEST IS " + num2);
+        } else {
+            System.out.println("THE GREATEST IS " + num3);
         }
     }
 }
